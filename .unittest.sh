0ece9ece7b866021bf897da504a5754794c18518
Luke Skibinski <l.skibinski@elifesciences.org>
2016-05-19 18:03:47 +0100

    this project must have at least 30% coverage to pass testing

--- .unittest.sh
+++ .unittest.sh
@@ -7,3 +7,11 @@ fi
 nosetests tests"$module" --config .noserc
 echo "* passed unittests!"
 coverage report
+
+covered=$(coverage report | grep TOTAL | awk '{print $6}' | sed 's/%//')
+if [ $covered -le 30 ]; then
+    echo
+    echo "FAILED this project requires at least 30% coverage"
+    echo
+    exit 1
+fi
