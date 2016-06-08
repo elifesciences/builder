ca9aa5d767d4bfaa9ec81e1b59378a2ad02813aa
Luke Skibinski <l.skibinski@elifesciences.org>
2016-06-08 09:39:49 +0100

    switched tests over to using green from nosetests. installs faster, nicer to look at, nicer to invocate. added tox config as well

--- .unittest.sh
+++ .unittest.sh
@@ -4,12 +4,11 @@ module=''
 if [ ! -z "$@" ]; then
     module=".$@"
 fi
-nosetests tests"$module" --config .noserc
-echo "* passed unittests!"
-coverage report
+green --run-coverage src.tests"$module"
 
+# is only run if tests pass
 covered=$(coverage report | grep TOTAL | awk '{print $6}' | sed 's/%//')
-if [ $covered -le 30 ]; then
+if [ $covered -le 50 ]; then
     echo
     echo "FAILED this project requires at least 30% coverage"
     echo
