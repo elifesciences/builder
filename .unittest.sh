84029ce3852e13760198573125787185b2233073
Luke Skibinski <l.skibinski@elifesciences.org>
2016-06-29 18:06:32 +0100

    fixed up the test script so individual modules can be passed in.

--- .unittest.sh
+++ .unittest.sh
@@ -4,13 +4,15 @@ module=''
 if [ ! -z "$@" ]; then
     module=".$@"
 fi
-green --run-coverage src.tests"$module"
+
+export PYTHONPATH="src"
+green --run-coverage tests"$module"
 
 # is only run if tests pass
 covered=$(coverage report | grep TOTAL | awk '{print $6}' | sed 's/%//')
 if [ $covered -le 50 ]; then
     echo
-    echo "FAILED this project requires at least 30% coverage"
+    echo "FAILED this project requires at least 50% coverage"
     echo
     exit 1
 fi
