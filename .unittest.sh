4927b5ab41d4d8a482a91b84678f9cfc4479bb24
Luke Skibinski <l.skibinski@elifesciences.org>
2016-06-30 11:13:50 +0100

    coverage test not run if a specific test/module was targeted

--- .unittest.sh
+++ .unittest.sh
@@ -8,11 +8,14 @@ fi
 export PYTHONPATH="src"
 green --run-coverage tests"$module"
 
-# is only run if tests pass
-covered=$(coverage report | grep TOTAL | awk '{print $6}' | sed 's/%//')
-if [ $covered -le 50 ]; then
-    echo
-    echo "FAILED this project requires at least 50% coverage"
-    echo
-    exit 1
+# only test coverage if we running complete set of tests
+if [ -z "$module" ]; then
+    # is only run if tests pass
+    covered=$(coverage report | grep TOTAL | awk '{print $6}' | sed 's/%//')
+    if [ $covered -le 50 ]; then
+        echo
+        echo "FAILED this project requires at least 50% coverage"
+        echo
+        exit 1
+    fi
 fi
