56cf2c28be81a3dc2aa7d934b60c8ff2da429950
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-01 08:21:18 +0100

    Porting from old builder: skipping vagrant code if vagrant is not installed

--- update.sh
+++ update.sh
@@ -7,20 +7,22 @@ python .prerequisites.py $@
 # remove any old compiled python files
 find src/ -name '*.pyc' -delete
 
-# installs s3 auth plugin so we can pull boxes from a private s3 bucket
-if [ ! -f .no-vagrant-s3auth.flag ]; then
-    # vagrant plugin update ... doesn't work apparently
-    # just calling install does
-    vagrant plugin install vagrant-s3auth
+if which vagrant; then
+    # installs s3 auth plugin so we can pull boxes from a private s3 bucket
+    if [ ! -f .no-vagrant-s3auth.flag ]; then
+        # vagrant plugin update ... doesn't work apparently
+        # just calling install does
+        vagrant plugin install vagrant-s3auth
 
-    #if ! vagrant plugin list | grep vagrant-s3auth; then
-    #    vagrant plugin install vagrant-s3auth
-    #else
-    #    echo "found s3 auth plugin, looking for upgrades"
-    #    vagrant plugin update vagrant-s3auth
-    #fi
-else
-    echo "* the no-vagrant-s3auth flag has been set. skipping check."
+        #if ! vagrant plugin list | grep vagrant-s3auth; then
+        #    vagrant plugin install vagrant-s3auth
+        #else
+        #    echo "found s3 auth plugin, looking for upgrades"
+        #    vagrant plugin update vagrant-s3auth
+        #fi
+    else
+        echo "* the no-vagrant-s3auth flag has been set. skipping check."
+    fi
 fi
 
 # so we can do ./bldr ...
@@ -36,12 +38,14 @@ fi
 source .activate-venv.sh
 
 # download the basebox from s3 if vagrant is installed
-if [ ! -f .no-install-basebox.flag ]; then
-    if [ type vagrant &> /dev/null ]; then
-        vagrant add box s3://elife-builder/boxes/ elifesciences/basebox
+if which vagrant; then
+    if [ ! -f .no-install-basebox.flag ]; then
+        if [ type vagrant &> /dev/null ]; then
+            vagrant add box s3://elife-builder/boxes/ elifesciences/basebox
+        fi
+    else
+        echo "* the no-install-basebox flag is set. skipping check"
     fi
-else
-    echo "* the no-install-basebox flag is set. skipping check"
 fi
 
 printf "\nall done\n\n"
