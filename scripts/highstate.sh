155182b99330b9ab0de45174784c8db99954cd3b
Luke Skibinski <l.skibinski@elifesciences.org>
2016-07-13 16:31:16 +0100

    highstate now returns better output than just errors and warning and silence

--- scripts/highstate.sh
+++ scripts/highstate.sh
@@ -3,7 +3,7 @@ set -e # everything must pass
 set -u # no unbound variables
 
 echo "Executing salt highstate (provisioning)"
-sudo salt-call state.highstate --retcode-passthrough || {
+sudo salt-call state.highstate -l info --retcode-passthrough || {
     status=$?
     echo "Error provisioning, state.highstate returned: ${status}"
 }
