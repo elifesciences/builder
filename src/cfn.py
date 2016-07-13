9ecc21ad51dac28664e11776a53436a9a11aa96e
Luke Skibinski <l.skibinski@elifesciences.org>
2016-07-13 16:31:53 +0100

    bug, removed a mysterious 'none' that was making a default value an tuple

--- src/cfn.py
+++ src/cfn.py
@@ -42,7 +42,7 @@ def update_master():
 @requires_project
 def create_stack(pname):
     """creates a new CloudFormation template for the given project."""
-    default_instance_id = core_utils.ymd(), None
+    default_instance_id = core_utils.ymd()
     inst_id = utils.uin("instance id", default_instance_id)
     stackname = core.mk_stackname(pname, inst_id)
     more_context = {'instance_id': stackname}
