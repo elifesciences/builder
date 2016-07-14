a67f569b8f508aeed31faa4a196fb1c34736b19f
Luke Skibinski <l.skibinski@elifesciences.org>
2016-07-14 13:18:32 +0100

    still problems with the asyncchannel and orphaned salt-master processes. update-master script now focuses just on the master server and kills it whenever it's called (every 60seconds)

--- scripts/update-master.sh
+++ scripts/update-master.sh
@@ -6,27 +6,36 @@ set -e # everything must pass
 set -u # no unbound variables
 set -xv  # output the scripts and interpolated steps
 
+cd /opt/builder
+if [ ! -d /vagrant ]; then
+    git reset --hard
+    git clean -d --force
+    git pull --rebase
+fi
+
+# install/update any python requirements
+/bin/bash .activate-venv.sh
+
 # this  clones/pulls all repos and updates the master config
-cd /opt/builder/
 BLDR_ROLE=master ./bldr remote_master.refresh
 
 # kill any salt-thing that may be running
 # why? encountering an issue where there are multiple salt-master processes
 # running, or a salt-call process waiting on a response from a dead master
 # it's such a hack.
-sudo killall salt-call   || true
-sudo killall salt-minion || true
+#sudo killall salt-call   || true
+#sudo killall salt-minion || true
 sudo killall salt-master || true
 
-sleep 1 # give them a moment to die
+#sleep 1 # give them a moment to die
 
 service salt-master restart
-service salt-minion restart
+#service salt-minion restart
 
 # some health checking
 # https://docs.saltstack.com/en/latest/ref/modules/all/salt.modules.saltutil.html#salt.modules.saltutil.sync_all
-salt-call saltutil.sync_all -l trace
-if (! salt-call sys.list_modules | grep elife); then
-    echo "couldn't find the 'elife' module. master server is in a bad state"
-    exit 1
-fi
+#salt-call saltutil.sync_all -l trace
+#if (! salt-call sys.list_modules | grep elife); then
+#    echo "couldn't find the 'elife' module. master server is in a bad state"
+#    exit 1
+#fi
