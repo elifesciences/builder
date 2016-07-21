d8f597cba6154e5ad2efd7e9db43459b07aa1512
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-21 13:47:26 +0100

    Removing not assigned variable

--- src/aws.py
+++ src/aws.py
@@ -20,7 +20,6 @@ def find_region(stackname=None):
 
     if more than one region available, it will raise an EnvironmentError.
     until we have some means of supporting multiple regions, this is the best solution"""
-    region = None
     if stackname:
         pdata = core.project_data_for_stackname(stackname)
         return pdata['aws']['region']
