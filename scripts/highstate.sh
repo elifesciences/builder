10f1896efd0655db62e4d68daa61d3c61c4782d8
Luke Skibinski <l.skibinski@elifesciences.org>
2016-08-03 15:42:33 +0100

    highstate script, added a note-to-self

--- scripts/highstate.sh
+++ scripts/highstate.sh
@@ -2,6 +2,9 @@
 set -e # everything must pass
 set -u # no unbound variables
 
+# TODO? enabling this will kill any 'stuck' highstate calls
+# killall salt-call
+
 echo "Executing salt highstate (provisioning)"
 sudo salt-call --force-color state.highstate -l info --retcode-passthrough || {
     status=$?
