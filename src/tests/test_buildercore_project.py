09ea952ff7a6459390edc678a2cb99421b25bff5
Luke Skibinski <l.skibinski@elifesciences.org>
2016-08-03 19:03:15 +0100

    tests, shifted the project config testing out of test_buildercore_core into test_buildercore_project. fixed up some disabled tests.

--- src/tests/test_buildercore_project.py
+++ src/tests/test_buildercore_project.py
@@ -1,8 +1,11 @@
 from . import base
-import os
+import os, json
 from os.path import join
-from buildercore import config, project
+from buildercore import config, project, utils
+from buildercore.project import files as project_files
 from collections import OrderedDict
+from unittest import skip
+
 
 class TestProject(base.BaseCase):
     def setUp(self):
@@ -36,7 +39,82 @@ class TestProject(base.BaseCase):
         self.assertEqual(project.project_list(prj_loc_lst), expected)
 
 
+class TestProjectData(base.BaseCase):
+    def setUp(self):
+        self.dummy_yaml = join(self.fixtures_dir, 'projects', 'dummy-project.yaml')
+        self.dummy1_config = join(self.fixtures_dir, 'dummy1-project.json')
+        self.dummy2_config = join(self.fixtures_dir, 'dummy2-project.json')
+        self.dummy3_config = join(self.fixtures_dir, 'dummy3-project.json')
 
+    def tearDown(self):
+        pass
+        
+    def test_configurations(self):
+        expected = [
+            ('dummy1', self.dummy1_config),
+            ('dummy2', self.dummy2_config),
+            ('dummy3', self.dummy3_config),
+        ]
+        for pname, expected_path in expected:
+            expected_data = json.load(open(expected_path, 'r'))
+            project_data = project.project_data(pname)
+            project_data = utils.remove_ordereddict(project_data)
+            self.assertEqual(expected_data, project_data, 'failed %s' % pname)
+
+    # snippets
+
+    def test_merge_default_snippet(self):
+        "merging a snippet into the defaults ensures all projects get that new default"
+        # all projects now get 999 cpus. perfectly sane requirement.
+        snippet = {'defaults':
+                       {'vagrant': {
+                           'cpus': 999}}}
+        project_data = project_files.project_data('dummy1', self.dummy_yaml, [snippet])
+        project_data = utils.remove_ordereddict(project_data)
+
+        # load up the expected fixture and switch the value ...
+        expected_data = json.load(open(self.dummy1_config, 'r'))
+        expected_data['vagrant']['cpus'] = 999
+        # ... then compare to actual
+        self.assertEqual(project_data, expected_data)
+
+    def test_deep_merge_default_snippet(self):
+        "merging a snippet into the defaults ensures all projects get that new default, even if it's deeply nested"
+        # all projects now get 999 cpus. perfectly sane requirement.
+        snippet = {'defaults':
+                       {'aws': {
+                           'rds': {
+                               'subnets': ['subnet-baz']}}}}
+        project_data = project_files.project_data('dummy1', self.dummy_yaml, [snippet])
+        project_data = utils.remove_ordereddict(project_data)
+
+        # load up the expected fixture and switch the value ...
+        expected_data = json.load(open(self.dummy1_config, 'r'))
+        expected_data['aws'].get('rds', {}).get('subnets', [''])[0] = 'subnet-baz'
+        # ... then compare to actual
+        self.assertEqual(project_data, expected_data)
+
+    def test_merge_multiple_default_snippets(self):
+        """merging multiple overlapping snippets into the defaults 
+        ensures all projects get the new defaults"""
+        # all projects now get 999 cpus. perfectly sane requirement.
+        snippet = {'defaults':
+                       {'vagrant': {
+                           'cpucap': 10,  # to be overriden by the override
+                           'cpus': 999}}}
+        snippet2 = {'defaults':
+                        {'vagrant': {
+                            'cpucap': 111}}}
+        snippet_list = [snippet, snippet2]
+        project_data = project_files.project_data('dummy1', self.dummy_yaml, snippet_list)
+        project_data = utils.remove_ordereddict(project_data)
+        
+        expected_data = json.load(open(self.dummy1_config, 'r'))
+        expected_data['vagrant']['cpus'] = 999
+        expected_data['vagrant']['cpucap'] = 111        
+        self.assertEqual(project_data, expected_data)
+    
+        
 class TestMultiProjects(base.BaseCase):
     def setUp(self):
         loaded_config = {
