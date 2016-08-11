d76f84c58aed11966f7942a83d01ee1f111b1905
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-11 09:23:06 +0100

    Disabling wrong pylint error: distutils is correctly installed

--- .lint.sh
+++ .lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 # E1129=doesn't like Fabric's context manager
-pylint -E src/* salt/salt/_modules/* --disable=E1129
+pylint -E src/* --disable=E1129 --rcfile=.pylintrc
 echo "* passed initial lint!"
