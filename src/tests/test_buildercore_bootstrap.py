d3193d25d6496b18aa68e37c0fb1ff75af27eec9
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-26 13:57:51 +0100

    Small test for buildercore.bootstrap

---
+++ src/tests/test_buildercore_bootstrap.py
@@ -0,0 +1,14 @@
+from . import base
+from buildercore import bootstrap
+from mock import patch, call, MagicMock
+
+class TestBuildercoreBootstrap(base.BaseCase):
+    @patch('buildercore.core.boto_sqs_conn')
+    @patch('buildercore.core.boto_sns_conn')
+    def test_setup_sqs(self, boto_sns_conn, boto_sqs_conn):
+        sns = MagicMock()
+        boto_sns_conn.return_value = sns
+
+        bootstrap.setup_sqs('project-with-sqs--ci', {'search': ['articles', 'podcasts']}, 'us-east-1')
+
+        sns.subscribe_sqs_queue.assert_called()
