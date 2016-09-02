0fd059666a2fe1225fd529e0e1f01c1a53b568d8
Luke Skibinski <l.skibinski@elifesciences.org>
2016-09-02 17:48:56 +0100

    cleaned up the new-project script

--- scripts/new-project.sh
+++ scripts/new-project.sh
@@ -4,93 +4,73 @@ set -e  # everything must pass
 set -u  # no unbound variables
 set -xv # show interpolation
 
-pname=$1
-
-mkdir -p ./cloned-projects/
-cd ./cloned-projects/
-git clone https://github.com/elifesciences/builder-example-project "$pname"
-rm -rf "./$pname/.git/"
-sed -i "s/#Example \$appname/$appname/g" README.md
-
-
-
-
-exit
-
-
-#!/bin/bash
-set -e # everything must pass
-set -u # no unbound variables
-set -xv  # output the scripts and interpolated steps
 appname=$1
 
-rm -rf "$appname-formula"
+cd ./cloned-projects
 
+# rough check formula doesn't already exist
 if [ -d "$appname-formula" ]; then
     echo "directory $appname-formula already exists. quitting"
     exit 1
 fi
 
+# create/update example project
+if [ ! -d "builder-example-project" ]; then
+    git clone https://github.com/elifesciences/builder-example-project
+else
+    (
+        cd builder-example-project || exit
+        git reset --hard
+        git pull
+    )
+fi
 
-cp -R builder-example-project $appname-formula
-cd $appname-formula
+cp -R builder-example-project "$appname-formula"
+cd "$appname-formula"
 
 rm -rf .git
 rm README.md
 rm salt/README.md
 rm -rf salt/simple-project
 rm salt/pillar/simple-project.sls
-cp ../README.md ../LICENCE.txt .
 
-ebuilder=/home/luke/dev/python/elife-builder
+cp .README.template README.md
+rm .README.template
 
-cp -RL $ebuilder/salt/salt/$appname salt/$appname
-cp $ebuilder/salt/salt/top.sls salt/example.top
-cp $ebuilder/salt/pillar/top.sls salt/pillar/top.sls
-if [ -e $ebuilder/salt/pillar/$appname.sls ]; then
-    cp $ebuilder/salt/pillar/$appname.sls salt/pillar/$appname.sls
-else
-    touch salt/pillar/$appname.sls
-fi
+(
+    # jump into the formula's 'salt' root as a subshell (else shell lint cries)
+    cd salt || exit
 
-cd salt
-rm example.pillar
-ln -s pillar/$appname.sls example.pillar
-cd ..
+    # create an empty state file
+    mkdir "$appname"
+    #touch "$appname/init.sls"
+    echo "echo 'hello, world':
+    cmd.run" > "$appname/init.sls"
 
-echo "
-base:
+    # generate an example top salt file
+    echo "base:
     '*':
         - elife
-        - $appname" > ./salt/pillar/top.sls
+        - $appname" > ./example.top
 
+    # replace the example.pillar file with a generated+empty one
+    rm example.pillar
+    echo "$appname:
+    no: data" > "./pillar/$appname.sls"
+    ln -s "./pillar/$appname.sls" example.pillar
+
+    # generate an example top pillar file
+    echo "base:
+    '*':
+        - elife
+        - $appname" > ./pillar/top.sls
+)
+
+# init the repo
 git init
 git add .
 git ci -am "initial commit"
 
-# replace any pillar.sys calls with pillar.elife calls
-find salt/ -type f -exec sed -i 's/pillar\.sys\./pillar\.elife\./g' {} \;
-
-# replace any pillar.monitor calls with pillar.logging calls
-find salt/ -type f -exec sed -i 's/pillar\.monitor\./pillar\.elife\.logging\./g' {} \;
-
-find salt/ -type f -exec sed -i 's/mysql_root\./db_root\./g' {} \;
-
-# replace any pillar.elife.env calls with pillar.elife.FOO calls
-# the logic needs to be adjusted
-# UPDATE: decided to keep pillar.elife.env and deprecated pillar.elife.dev
-#find salt/ -type f -exec sed -i 's/pillar\.elife\.env/pillar\.elife\.FOO/g' {} \;
-
-# replace any logging.central_logging calls with logging.BAR calls
-# these need to be REMOVED
-find salt/ -type f -exec sed -i 's/logging\.central_logging/logging\.BAR/g' {} \;
-
-
-
 # render the readme template
 sed -i "s/\$appname/$appname/g" README.md
-
-cp salt/pillar/$appname.sls /home/luke/dev/salt/builder-private/pillar/$appname.sls.new
-
-
 
