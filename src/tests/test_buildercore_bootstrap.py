57f6911cadf1e057066418d895d75ab133f48593
Luke Skibinski <l.skibinski@elifesciences.org>
2016-09-14 18:22:58 +0100

    replaced constructor override with a setUp func

--- src/tests/test_buildercore_bootstrap.py
+++ src/tests/test_buildercore_bootstrap.py
@@ -4,28 +4,25 @@ import cfn
 from mock import patch, call, MagicMock
 
 class TestBuildercoreBootstrap(base.BaseCase):
-    def __init__(self, *args, **kwargs):
-        super(TestBuildercoreBootstrap, self).__init__(*args, **kwargs)
+    def setUp(self):
         self.stacknames = []
-
-    @patch('buildercore.core.boto_sqs_conn')
-    @patch('buildercore.core.boto_sns_conn')
-    def test_setup_sqs(self, boto_sns_conn, boto_sqs_conn):
-        sns = MagicMock()
-        boto_sns_conn.return_value = sns
-
-        bootstrap.setup_sqs('project-with-sqs--ci', {'search': ['articles', 'podcasts']}, 'us-east-1')
-
-        sns.subscribe_sqs_queue.assert_called()
-
-    def test_create(self):
-        stackname = 'dummy1--test'
-        self.stacknames.append(stackname)
-
-        cfngen.generate_stack('dummy1', stackname=stackname)
-        bootstrap.create_stack(stackname)
 
     def tearDown(self):
         for stackname in self.stacknames:
             cfn.ensure_destroyed(stackname)
 
+    @patch('buildercore.core.boto_sqs_conn')
+    @patch('buildercore.core.boto_sns_conn')
+    def test_setup_sqs(self, boto_sns_conn, boto_sqs_conn):
+        "an sqs-enabled project can be created and bootstrapped"
+        sns = MagicMock()
+        boto_sns_conn.return_value = sns
+        bootstrap.setup_sqs('project-with-sqs--ci', {'search': ['articles', 'podcasts']}, 'us-east-1')
+        sns.subscribe_sqs_queue.assert_called()
+
+    def test_create(self):
+        stackname = 'dummy1--test'
+        self.stacknames.append(stackname) # ensures stack is destroyed
+
+        cfngen.generate_stack('dummy1', stackname=stackname)
+        bootstrap.create_stack(stackname)
