0dc5ffc4178d2a1d3da718ae945b7b0d30fe6107
Giorgio Sironi <g.sironi@elifesciences.org>
2016-09-21 12:02:05 +0100

    Porting buildvars to work on multiple nodes
    In particular, the `switch_revision_update_instance` task is necessary to deploy on stacks with multiple EC2 nodes

--- src/buildvars.py
+++ src/buildvars.py
@@ -1,9 +1,10 @@
 from buildercore.bvars import encode_bvars, read_from_current_host
 from fabric.api import sudo, put, hide
 from StringIO import StringIO
-from decorators import echo_output, requires_aws_stack, debugtask
-from buildercore.core import stack_conn
+from decorators import requires_aws_stack, debugtask
+from buildercore.core import stack_all_ec2_nodes
 from buildercore import utils as core_utils
+from pprint import pprint
 import utils
 import logging
 LOG = logging.getLogger(__name__)
@@ -13,33 +14,37 @@ OLD, ABBREV, FULL = 'old', 'abbrev', 'full'
 @debugtask
 @requires_aws_stack
 def switch_revision(stackname, revision=None):
-    buildvars = _retrieve_build_vars(stackname)
     if revision is None:
         revision = utils.uin('revision', None)
 
-    if 'revision' in buildvars and revision == buildvars['revision']:
-        print 'FYI, the instance is already on that revision!'
-        return
+    def _switch_revision_single_ec2_node():
+        buildvars = _retrieve_build_vars()
 
-    new_data = buildvars
-    new_data['revision'] = revision
-    _update_remote_bvars(stackname, new_data)    
+        if 'revision' in buildvars and revision == buildvars['revision']:
+            print 'FYI, the instance is already on that revision!'
+            return
+
+        new_data = buildvars
+        new_data['revision'] = revision
+        _update_remote_bvars(stackname, new_data)    
+
+    stack_all_ec2_nodes(stackname, _switch_revision_single_ec2_node)
 
 @debugtask
 @requires_aws_stack
-@echo_output
 def read(stackname):
     "returns the unencoded build variables found on given instance"
-    with stack_conn(stackname):
-        return read_from_current_host()
+    return stack_all_ec2_nodes(stackname, lambda: pprint(read_from_current_host()))
 
 @debugtask
 @requires_aws_stack
-@echo_output
 def valid(stackname):
+    return stack_all_ec2_nodes(stackname, lambda: pprint(_validate()))
+
+def _validate():
     "returns a pair of (type, build data) for the given instance. type is either 'old', 'abbrev' or 'full'"
     try:
-        buildvars = read(stackname)
+        buildvars = read_from_current_host()
         bvarst = _bvarstype(buildvars)
         assert bvarst in [None, OLD, ABBREV, FULL], \
           "the build vars found were structured unfamiliarly"
@@ -52,21 +57,22 @@ def valid(stackname):
 
 @debugtask
 @requires_aws_stack
-@echo_output
 def fix(stackname):
-    bvarst, _ = valid(stackname)
-    if bvarst is None:
-        LOG.info("no build vars found, adding defaults")
-        new_vars = {'branch': 'master', 'revision': None}
-        _update_remote_bvars(stackname, new_vars)
-    else:
-        LOG.info("valid bvars found (%s), no fix necessary", bvarst)
+    def _fix_single_ec2_node():
+        bvarst, _ = _validate()
+        if bvarst is None:
+            LOG.info("no build vars found, adding defaults")
+            new_vars = {'branch': 'master', 'revision': None}
+            _update_remote_bvars(stackname, new_vars)
+        else:
+            LOG.info("valid bvars found (%s), no fix necessary", bvarst)
 
-def _retrieve_build_vars(stackname):
-    #pdata = core.project_data_for_stackname(stackname)
+    stack_all_ec2_nodes(stackname, _fix_single_ec2_node)
+
+def _retrieve_build_vars():
     print 'looking for build vars ...'
     with hide('everything'):
-        bvarst, buildvars = valid(stackname)
+        bvarst, buildvars = _validate()
     assert bvarst in [ABBREV, FULL], \
       "the build-vars.json file for %r is not valid. use `./bldr buildvars.fix` to attempt to fix this."
     print 'found build vars'
@@ -76,16 +82,16 @@ def _retrieve_build_vars(stackname):
 def _update_remote_bvars(stackname, buildvars):
     LOG.info('updating %r with new vars %r',stackname, buildvars)
     assert core_utils.hasallkeys(buildvars, ['branch']) #, 'revision']) # we don't use 'revision'
-    with stack_conn(stackname):
-        encoded = encode_bvars(buildvars)
-        fid = core_utils.ymd(fmt='%Y%m%d%H%M%S')
-        cmds = [
-            # make a backup
-            'if [ -f /etc/build-vars.json.b64 ]; then cp /etc/build-vars.json.b64 /tmp/build-vars.json.b64.%s; fi;' % fid,
-        ]
-        map(sudo, cmds)
-        put(StringIO(encoded), "/etc/build-vars.json.b64", use_sudo=True)
-        LOG.info("%r updated", stackname)            
+
+    encoded = encode_bvars(buildvars)
+    fid = core_utils.ymd(fmt='%Y%m%d%H%M%S')
+    cmds = [
+        # make a backup
+        'if [ -f /etc/build-vars.json.b64 ]; then cp /etc/build-vars.json.b64 /tmp/build-vars.json.b64.%s; fi;' % fid,
+    ]
+    map(sudo, cmds)
+    put(StringIO(encoded), "/etc/build-vars.json.b64", use_sudo=True)
+    LOG.info("%r updated", stackname)
 
 
 def _bvarstype(bvars):
