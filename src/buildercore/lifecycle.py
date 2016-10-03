9b31d04e0ba4be3d941cfa0a17a5ca014baa8205
Giorgio Sironi <g.sironi@elifesciences.org>
2016-10-03 12:49:48 +0100

    Added stop_if_next_hour_is_imminent task

--- src/buildercore/lifecycle.py
+++ src/buildercore/lifecycle.py
@@ -2,6 +2,7 @@
 
 The primary reason for doing this is to save on costs."""
 
+from datetime import datetime
 import logging
 from .core import connect_aws_with_stack
 from .utils import call_while, die
@@ -21,7 +22,7 @@ def start(stackname):
 
     LOG.info("Nodes to be started: %s", to_be_started)
     _connection(stackname).start_instances(to_be_started)
-    _ensure_all_in_state('running', stackname)
+    _wait_all_in_state(stackname, 'running', to_be_started)
 
 def stop(stackname):
     "Puts all EC2 nodes of stackname into the 'stopped' state. Idempotent"
@@ -36,9 +37,32 @@ def stop(stackname):
 
     LOG.info("Nodes to be stopped: %s", to_be_stopped)
     _connection(stackname).stop_instances(to_be_stopped)
-    _ensure_all_in_state('stopped', stackname)
+    _wait_all_in_state(stackname, 'stopped', to_be_stopped)
 
-def _ensure_all_in_state(state, stackname):
+def last_start_time(stackname):
+    nodes = _nodes(stackname, state='running')
+    def _parse_datetime(value):
+        return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
+    return {node.id:_parse_datetime(node.launch_time) for node in nodes}
+
+def stop_if_next_hour_is_imminent(stackname, minimum_minutes=55):
+    maximum_minutes = 60
+    starting_times = last_start_time(stackname)
+    running_times = {node_id:int((datetime.utcnow() - launch_time).total_seconds()) % (maximum_minutes * 60) for (node_id, launch_time) in starting_times.iteritems()}
+    LOG.info("Hourly fraction running times: %s", running_times)
+
+    minimum_running_time = minimum_minutes * 60
+    maximum_running_time = maximum_minutes * 60
+    LOG.info("Interval to select nodes to stop: %s-%s", minimum_running_time, maximum_running_time)
+
+    to_be_stopped = [node_id for (node_id, running_time) in running_times.iteritems() if running_time >= minimum_running_time]
+    LOG.info("Selected for stopping: %s", to_be_stopped)
+    if to_be_stopped:
+        _connection(stackname).stop_instances(to_be_stopped) 
+        _wait_all_in_state(stackname, 'stopped', to_be_stopped)
+
+
+def _wait_all_in_state(stackname, state, node_ids):
     def some_node_is_still_not_compliant():
         return set(_nodes_states(stackname).values()) != {state}
     # TODO: timeout argument
@@ -54,22 +78,27 @@ def _select_nodes_with_state(interesting_state, states):
     return [instance_id for (instance_id, state) in states.iteritems() if state == interesting_state]
 
 
-def _nodes_states(stackname):
+def _nodes_states(stackname, node_ids=None):
     """dictionary from instance id to a string state.
     
     e.g. {'i-6f727961': 'stopped'}"""
-    return {node.id:node.state for node in _nodes(stackname)}
+    return {node.id:node.state for node in _nodes(stackname, node_ids=node_ids)}
 
-def _nodes(stackname):
+def _nodes(stackname, state=None, node_ids=None):
     "returns list of ec2 instances data for a *specific* stackname"
     conn = _connection(stackname)
-    return conn.get_only_instances(filters=_all_nodes_filter(stackname))
+    return conn.get_only_instances(filters=_all_nodes_filter(stackname, state=state, node_ids=node_ids))
 
-def _all_nodes_filter(stackname):
-    return {
+def _all_nodes_filter(stackname, state, node_ids):
+    query = {
         'tag-key': ['Cluster', 'Name'],
         'tag-value': [stackname],
     }
+    if state:
+        query['instance-state-name'] = [state]
+    if node_ids:
+        query['instance-id'] = node_ids
+    return query
 
 def _connection(stackname):
     return connect_aws_with_stack(stackname, 'ec2')
--- src/lifecycle.py
+++ src/lifecycle.py
@@ -1,6 +1,6 @@
 from fabric.api import task
 from buildercore import lifecycle
-from decorators import requires_aws_stack, timeit
+from decorators import requires_aws_stack, timeit, echo_output
 
 @task
 @requires_aws_stack
@@ -13,3 +13,19 @@ def start(stackname):
 @timeit
 def stop(stackname):
     lifecycle.stop(stackname)
+
+@task
+@requires_aws_stack
+@echo_output
+def last_start_time(stackname):
+    return lifecycle.last_start_time(stackname)
+
+@task
+@requires_aws_stack
+@timeit
+def stop_if_next_hour_is_imminent(stackname, minimum_minutes='55'):
+    # TODO: can we write a description of the @task somewhere?
+    """If a node has been running for a time between X:55:00 and X:59:59 hours, stops it to avoid incurring in a new charge for the next hour.
+    
+    The assumption is that stacks where this command is used are not needed for long parts of the day/week, and that who needs them will call the start task first."""
+    return lifecycle.stop_if_next_hour_is_imminent(stackname, int(minimum_minutes))
