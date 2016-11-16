27fc2a16bcd65267deb9c85dd3317c72f7f30047
Luke Skibinski <l.skibinski@elifesciences.org>
2016-11-16 16:06:12 +0000

    lax task, cleaned up dumpdb, renamed it

--- src/lax.py
+++ src/lax.py
@@ -1,20 +1,31 @@
-from fabric.api import run, task, get, local
+from fabric.api import run, task, get
+from fabric.context_managers import cd
 from decorators import requires_aws_project_stack
-from buildercore.core import stack_conn
+from buildercore.core import stack_conn, utils as core_utils
 
-@task
+@task(alias='dumpdata')
 @requires_aws_project_stack('lax')
-def dumpdata(stackname):
-    with stack_conn(stackname):
-        cmds = [
-            'cd /srv/lax/ && ./manage.sh dumpdata --exclude=contenttypes --natural-foreign --natural-primary --indent=4 | gzip -9 - > /tmp/lax-db.json.gz',
-        ]
-        map(run, cmds)
-        get('/tmp/lax-db.json.gz', 'public/lax-db.json.gz')
-        local('gunzip public/lax-db.json.gz')
+def download_db_fixtures(stackname):
+    """downloads a dump of the lax database as *django fixtures* to the 
+    current directory as 'stackname-yyyymmddhhmmss'.json.gz"""
 
-        print '\ndata dumped to `public/lax-db.json`\n'
-        return "public/lax-db.json"
+    dtstamp = core_utils.utcnow().isoformat().rsplit('.',1)[0].replace(':', '-')
+    local_path = "./%s.%s.json.gz" % (stackname, dtstamp)
+    remote_path = '/tmp/db.json.gz'
+    
+    with stack_conn(stackname), cd('/srv/lax/'):
+        # dump fixtures
+        run('./manage.sh dumpdata --exclude=contenttypes --natural-foreign --natural-primary --indent=4 | ' \
+            'gzip -9 - > ' + remote_path)
+
+        # download
+        get(remote_path, local_path)
+
+        # remove remote dump
+        run('rm ' + remote_path)
+
+        print '\ndata dumped to `%s`\n' % local_path
+        return local_path
 
 '''
 @task
