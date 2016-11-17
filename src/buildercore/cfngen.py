d86b0eb8bce65efefa619765b06db3910a851256
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-17 09:48:39 +0000

    s3 to sqs provisioning is not working anymore, or left half-way done

--- src/buildercore/cfngen.py
+++ src/buildercore/cfngen.py
@@ -130,10 +130,11 @@ def build_context(pname, **more_context): # pylint: disable=too-many-locals
                 subscriptions.append(_parameterize(topic_template_name))
         context['sqs'][queue_name] = subscriptions
 
-    if 's3' in context['project']['aws']['s3']:
-        # at the moment, don't support any parameterization of names,
-        # but if we start using {instance}, here is the place to replace it
-        context['s3'] = context['project']['aws']['s3']
+    # at the moment, don't support any parameterization of names,
+    # but if we start using {instance}, here is the place to replace it
+    # - look at context['project']['aws']['s3']
+    # - build what is necessary for buildercore.bootstrap.setup_s3()
+    context['s3'] = [] 
 
     return context
 
