ebd34b1763dc27dfd88bb5860666b8decdd372c3
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-22 13:44:59 +0000

    Using buildercore.utils.missingkeys

--- src/buildvars.py
+++ src/buildvars.py
@@ -54,11 +54,11 @@ def _validate():
             'build vars not found (%s). use `./bldr buildvars.fix` to attempt to fix this.',
             buildvars
         )
-        for key in ['stackname', 'instance_id', 'branch', 'revision', 'is_prod_instance']:
-            core_utils.ensure(
-                key in buildvars,
-                'build vars are not valid: missing key %s. use `./bldr buildvars.fix` to attempt to fix this.' % key
-            )
+        missing_keys = core_utils.missingkeys(buildvars, ['stackname', 'instance_id', 'branch', 'revision', 'is_prod_instance'])
+        core_utils.ensure(
+            len(missing_keys) == 0,
+            'build vars are not valid: missing keys %s. use `./bldr buildvars.fix` to attempt to fix this.' % missing_keys
+        )
         return buildvars
 
     except (ValueError, AssertionError) as ex:
