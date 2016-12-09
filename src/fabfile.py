8813e60a1a8802d1d45e3f87f6bd54b1e622ca86
Giorgio Sironi <g.sironi@elifesciences.org>
2016-12-09 16:53:20 +0000

    Experimenting with a sleep task

--- src/fabfile.py
+++ src/fabfile.py
@@ -7,6 +7,10 @@ SRC_DIR = os.path.dirname(os.path.abspath(__file__)) # elife-builder/src/
 # once called 'THIS_DIR', now deprecated as confusing.
 PROJECT_DIR = os.path.dirname(SRC_DIR)    # elife-builder/
 
+import monitoring
+import fabric.api
+fabric.api.task = monitoring.task
+
 from cfn import *
 
 # aws tasks are not working for some reason.. possibly circular dependency
