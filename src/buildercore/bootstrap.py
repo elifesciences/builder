5e889ac85f3cda27e092d59ea2ed2509e7ee5bc9
Giorgio Sironi <g.sironi@elifesciences.org>
2017-03-30 16:11:54 +0100

    Apparently Cloudfront distributions take a long time to be created, at least 20 minutes

--- src/buildercore/bootstrap.py
+++ src/buildercore/bootstrap.py
@@ -108,7 +108,7 @@ def _wait_until_in_progress(stackname):
         stack_status = core.describe_stack(stackname).stack_status
         LOG.info("Stack status: %s", stack_status)
         return stack_status in ['CREATE_IN_PROGRESS']
-    utils.call_while(partial(is_updating, stackname), timeout=1200, update_msg='Waiting for AWS to finish creating stack ...')
+    utils.call_while(partial(is_updating, stackname), timeout=3600, update_msg='Waiting for AWS to finish creating stack ...')
     final_stack = core.describe_stack(stackname)
     events = [(e.resource_status, e.resource_status_reason) for e in final_stack.describe_events()]
     ensure(final_stack.stack_status in ['CREATE_COMPLETE'], "Final stack status is not successful: %s.\nEvents: %s", final_stack.stack_status, pformat(events))
