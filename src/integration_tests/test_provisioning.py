6b4531a582e3f6821ad6362e884616a3cdb87c4e
Luke Skibinski <l.skibinski@elifesciences.org>
2017-06-05 11:27:16 +0930

    added cleanup after integration tests

--- src/integration_tests/test_provisioning.py
+++ src/integration_tests/test_provisioning.py
@@ -8,6 +8,8 @@ from buildercore.config import BOOTSTRAP_USER
 import buildvars
 import cfn
 
+from fabfile import PROJECT_DIR
+
 def generate_environment_name():
     """to avoid multiple people clashing while running their builds
        and new builds clashing with older ones"""
@@ -21,6 +23,21 @@ class TestProvisioning(base.BaseCase):
     def tearDown(self):
         for stackname in self.stacknames:
             cfn.ensure_destroyed(stackname)
+
+        tempfiles = [
+            'ls',
+            'venv/bin/less',
+            'subfolder/pwd',
+            'subfolder',
+        ]
+        for tf in tempfiles:
+            path = os.path.join(PROJECT_DIR, tf)
+            if os.path.isfile(path):
+                os.unlink(path)
+            elif os.path.isdir(path):
+                # assumes dir is empty
+                os.rmdir(path)
+            self.assertFalse(os.path.exists(path), "failed to delete path %r in tearDown" % path)
 
     def test_create(self):
         with settings(abort_on_prompts=True):
@@ -40,13 +57,15 @@ class TestProvisioning(base.BaseCase):
             lifecycle.start(stackname)
 
             cfn.cmd(stackname, "ls -l /", username=BOOTSTRAP_USER, concurrency='parallel')
-            cfn.download_file(stackname, "/bin/ls", "ls", use_bootstrap_user="true")
-            self.assertTrue(os.path.isfile("./ls"))
-            cfn.download_file(stackname, "/bin/less", "venv/bin/", use_bootstrap_user="true")
-            self.assertTrue(os.path.isfile("./venv/bin/less"))
 
-            cfn.download_file(stackname, "/bin/pwd", "subfolder/pwd", use_bootstrap_user="true")
-            self.assertTrue(os.path.isfile("./subfolder/pwd"))
+            dl_list = [
+                ('/bin/ls', 'ls'),
+                ('/bin/less', 'venv/bin/'),
+                ('/bin/pwd', 'subfolder/pwd'),
+            ]
+            for dl, target in dl_list:
+                cfn.download_file(stackname, dl, target, use_bootstrap_user="true")
+                self.assertTrue(os.path.isfile(target), "failed to download %r, given target is not a file" % target)
 
 class TestDeployment(base.BaseCase):
     def setUp(self):
