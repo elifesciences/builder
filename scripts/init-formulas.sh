3104ba005eeef858bbe218ddc9974e33a319131d
Luke Skibinski <l.skibinski@elifesciences.org>
2017-08-16 15:29:46 +0930

    init-formulas script now has salt paths in correct order

--- scripts/init-formulas.sh
+++ scripts/init-formulas.sh
@@ -6,6 +6,79 @@
 set -eu
 set -x
 
+# space separated string of repositories to clone as a single argument
+# "https://github.com/elifesciences/builder-base-formula https://github.com/elifesciences/api-dummy-formula"
+formula_list=$1
+pillar_repo=$2 # what secrets do I know?
+
+# clone the private repo (whatever it's name is) into /opt/builder-private/
+# REQUIRES CREDENTIALS!
+if [ ! -d /opt/builder-private ]; then
+    cd /opt
+    git clone "$pillar_repo" builder-private || {
+        set +xv
+        echo "
+----------
+
+could not clone your 'builder-private' repository:
+    $pillar_repo
+
+this means you're stuck using the dummy pillar data in builder-base-formula until
+the 'builder-private' repo can be cloned and configured.
+
+when ready, complete this process by using the 'masterless.update' command:
+
+    ./bldr masterless.update:<stackname>
+
+----------"
+
+        exit 0
+    }
+    
+else
+    cd /opt/builder-private
+    git clean -d --force # in vagrant, destroys any rsync'd files
+    git reset --hard
+    git pull
+fi
+
+# we can't use the master-server's top.sls file (and it probably shouldn't be
+# there as 'top.sls' anyway), so delete it. 
+rm -f /opt/builder-private/salt/top.sls
+
+
+#
+# handle formulas
+#
+
+formula_dir="/opt/formulas"
+
+# the /srv/salt/ directory is where we'll link the top.sls files
+# they are otherwise empty
+mkdir -p "$formula_dir" /srv/salt/ /srv/salt/pillar/
+
+echo "---
+file_client: local
+log_level: info
+fileserver_backend:
+- roots" > /etc/salt/minion
+
+echo "file_roots:
+  base:
+    - /srv/salt/
+    - /opt/builder-private/salt/" > /etc/salt/minion.d/file_roots.conf
+
+# we only care about the pillar data in builder-private
+echo "pillar_roots:
+  base:
+    - /srv/salt/pillar/
+    - /opt/builder-private/pillar/" > /etc/salt/minion.d/pillar_roots.conf
+
+# convenience. 
+# this won't do anything but put the two top.sls files closer to each other
+cd /srv/salt/pillar
+ln -sfT /opt/builder-private/pillar/top.sls top.sls
+
 clone_update() {
     repo=$1
     repo_name=${repo##*/}
@@ -23,80 +96,17 @@ clone_update() {
 
     # tell the minion where it can find the formula's state and pillar data
     echo "    - $repo_path/salt/" >> /etc/salt/minion.d/file_roots.conf
-    echo "    - $repo_path/salt/pillar/" >> /etc/salt/minion.d/pillar_roots.conf
 
     topfile="$repo_path/salt/example.top"
 
     # successively overwrites the top file until the last one (project formula) wins
     if [ -e "$topfile" ]; then
-        cd "$repo_path/salt"; ln -sfT "$topfile" top.sls
+        cd /srv/salt 
+        ln -sfT "$topfile" top.sls
     fi
 }
-
-# space separated string of repositories to clone as a single argument
-# "https://github.com/elifesciences/builder-base-formula https://github.com/elifesciences/api-dummy-formula"
-formula_list=$1
-pillar_repo=$2 # what secrets do I know?
-
-#
-# handle formulas
-#
-
-formula_dir="/opt/formulas"
-
-mkdir -p "$formula_dir"
-
-echo "---
-file_client: local
-log_level: info
-fileserver_backend:
-- roots" > /etc/salt/minion
-
-printf "file_roots:\n  base:\n" > /etc/salt/minion.d/file_roots.conf
-printf "pillar_roots:\n  base:\n" > /etc/salt/minion.d/pillar_roots.conf
 
 for formula in $formula_list; do
     clone_update "$formula"
 done
-
-#
-# handle the private repo
-#
-
-# clone the private repo (whatever it's name is) into /opt/builder-private/
-# REQUIRES CREDENTIALS!
-if [ ! -d /opt/builder-private ]; then
-    cd /opt
-    git clone "$pillar_repo" builder-private || {
-        set +xv
-        pubkey=$(cat /root/.ssh/id_rsa.pub)
-        echo "
-----------
-
-could not clone your 'builder-private' repository:
-    $pillar_repo
-
-this means you're stuck using the dummy pillar data in builder-base-formula until
-the 'builder-private' repo can be cloned and configured.
-
-when ready, complete this process by using the 'masterless.update' command:
-
-    ./bldr masterless.update:$stackname
-
-----------"
-
-        exit 0
-    }
-    
-else
-    cd /opt/builder-private
-    git clean -d --force # in vagrant, destroys any rsync'd files
-    git reset --hard
-    git pull
-fi
-
-
-# TODO: replace builder-base entry in /etc/salt/minion.d/pillar_roots
-# TODO: add entry in /etc/salt/minion.d/file_roots
-
 
