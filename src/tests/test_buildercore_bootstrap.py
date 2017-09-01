9337538ea5ec4a4101282cd29c13ce98ad762a5e
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-01 11:27:53 +0100

    Linting

--- src/tests/test_buildercore_bootstrap.py
+++ src/tests/test_buildercore_bootstrap.py
@@ -4,9 +4,9 @@ from buildercore import bootstrap
 class TestBuildercoreBootstrap(base.BaseCase):
     def test_master_configuration(self):
         formulas = ['https://github.com/elifesciences/journal-formula', 'https://github.com/elifesciences/lax-formula']
-        master_configuration_template=open('src/tests/fixtures/etc-salt-master.template', 'r')
+        master_configuration_template = open('src/tests/fixtures/etc-salt-master.template', 'r')
         master_configuration = bootstrap.render_master_configuration(master_configuration_template, formulas).getvalue()
-	expected_configuration = """auto_accept: true
+        expected_configuration = """auto_accept: true
 interface: 0.0.0.0
 log_level: info
 fileserver_backend:
@@ -22,4 +22,3 @@ pillar_roots:
     - /opt/builder-private/pillar
 """
         self.assertEqual(master_configuration, expected_configuration)
-
