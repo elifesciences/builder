5bab96b2f293da57eb1a4bd820cca6d1dea1c441
Luke Skibinski <l.skibinski@elifesciences.org>
2017-09-04 12:34:59 +0930

    fixed up bootstrap master configuration test

--- src/tests/test_buildercore_bootstrap.py
+++ src/tests/test_buildercore_bootstrap.py
@@ -1,11 +1,13 @@
 from . import base
 from buildercore import bootstrap
+from buildercore.utils import yaml_dumps
 
 class TestBuildercoreBootstrap(base.BaseCase):
     def test_master_configuration(self):
         formulas = ['https://github.com/elifesciences/journal-formula', 'https://github.com/elifesciences/lax-formula']
         master_configuration_template = open('src/tests/fixtures/etc-salt-master.template', 'r')
-        master_configuration = bootstrap.render_master_configuration(master_configuration_template, formulas).getvalue()
+        master_configuration = bootstrap.expand_master_configuration(master_configuration_template, formulas)
+        master_configuration_yaml = yaml_dumps(master_configuration)
         expected_configuration = """auto_accept: true
 interface: 0.0.0.0
 log_level: info
@@ -21,4 +23,4 @@ pillar_roots:
     base:
     - /opt/builder-private/pillar
 """
-        self.assertEqual(master_configuration, expected_configuration)
+        self.assertEqual(master_configuration_yaml, expected_configuration)
