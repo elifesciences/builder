f6ad4c2728c66e3efcc3cbbe136e76d28d2f257a
Luke Skibinski <l.skibinski@elifesciences.org>
2017-09-04 12:30:19 +0930

    shifted .master.py logic into .project.py wrapped calls to .project.py in Vagrantfile to reduce duplication shifted the rendering of master config to YAML out of bootstrap.py. no need to mix concerns here. renamed render_master_configuration to expand_master_configuration gave expand_master_configuration a sensible default for formula list

--- .master.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# renders a master-server's configuration as YAML
-# only argument is the template etc-salt-master file
-import sys
-from buildercore import bootstrap, project
-
-all_formulas = project.known_formulas()
-master_configuration_template=open(sys.argv[1], 'r')
-print bootstrap.render_master_configuration(master_configuration_template, all_formulas).getvalue()
