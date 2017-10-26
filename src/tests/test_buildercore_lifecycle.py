f8ebe858ac5d63d5fb8ac96636f5b1913bfd850a
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-26 14:35:03 +0100

    Taking advantage of data generation from _ec2_instance

--- src/tests/test_buildercore_lifecycle.py
+++ src/tests/test_buildercore_lifecycle.py
@@ -25,26 +25,13 @@ class TestBuildercoreLifecycle(base.BaseCase):
     @patch('buildercore.lifecycle.find_rds_instances')
     @patch('buildercore.lifecycle.find_ec2_instances')
     def test_start_idempotence(self, find_ec2_instances, find_rds_instances):
-        some = MagicMock()
-        some.id = 'i-456'
-        some.state = 'running'
-        some.tags = {'Name': 'dummy1--test--1'}
-        some.launch_time = '2000-01-01T00:00:00.000Z'
-
-        find_ec2_instances.return_value = [some]
+        find_ec2_instances.return_value = [self._ec2_instance('running')]
         lifecycle.start('dummy1--test')
 
     @patch('buildercore.lifecycle.find_ec2_instances')
     def test_ignores_terminated_stacks_that_have_been_replaced_by_a_new_instance(self, find_ec2_instances):
-        old = MagicMock()
-        old.id = 'i-123'
-        old.state = 'terminated'
-        old.tags = {'Name': 'dummy1--test--1'}
-
-        new = MagicMock()
-        new.id = 'i-456'
-        new.state = 'running'
-        new.tags = {'Name': 'dummy1--test--1'}
+        old = self._ec2_instance('terminated', 'i-123')
+        new = self._ec2_instance('running', 'i-456')
 
         find_ec2_instances.return_value = [old, new]
         self.assertEqual({'i-456': 'running'}, lifecycle._ec2_nodes_states('dummy1--test'))
@@ -52,20 +39,15 @@ class TestBuildercoreLifecycle(base.BaseCase):
     @patch('buildercore.lifecycle._stop')
     @patch('buildercore.lifecycle.find_ec2_instances')
     def test_stops_instances_when_running_for_too_many_minutes(self, find_ec2_instances, _stop):
-        some = MagicMock()
-        some.id = 'i-456'
-        some.state = 'running'
-        some.tags = {'Name': 'dummy1--test--1'}
-        some.launch_time = '2000-01-01T00:00:00.000Z'
 
-        find_ec2_instances.return_value = [some]
+        find_ec2_instances.return_value = [self._ec2_instance('running', launch_time='2000-01-01T00:00:00.000Z')]
         lifecycle.stop_if_running_for('dummy1--test', 30)
 
-    def _ec2_instance(self, state='running'):
+    def _ec2_instance(self, state='running', id='i-456', launch_time='2017-01-01T00:00:00.000Z'):
         instance = MagicMock()
-        instance.id = 'i-456'
+        instance.id = id
         instance.state = state
         instance.tags = {'Name': 'dummy1--test--1'}
-        instance.launch_time = '2000-01-01T00:00:00.000Z'
+        instance.launch_time = launch_time
         return instance
 
