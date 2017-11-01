b27891f20e3b405d4fa21447cb4f8f5f3c66a5e6
Luke Skibinski <l.skibinski@elifesciences.org>
2017-11-01 10:47:15 +1030

    bad ec2 buildvar breaks ssh access to instance

--- src/cfn.py
+++ src/cfn.py
@@ -205,7 +205,15 @@ def _are_there_existing_servers(context):
         # very old stack, canned response
         return True
 
-    return context['ec2'] and len(context['ec2'].get('suppressed', [])) < context['ec2'].get('cluster-size', 1)
+    if isinstance(context['ec2'], bool):
+        LOG.error("bad buildvars in %s: context.ec2 is a boolean?" % context['full_hostname'], extra={'context': str(context)})
+        return True
+    
+    from pprint import pprint
+    pprint(context)
+    num_suppressed = len(context['ec2'].get('suppressed', []))
+    cluster_size = context['ec2'].get('cluster-size', 1)
+    return context['ec2'] and num_suppressed < cluster_size
 
 def _check_want_to_be_running(stackname, autostart=False):
     try:
