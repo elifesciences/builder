3e75f84868fd4768fc96a272022eafa2aab82aff
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-20 15:04:52 +0000

    Limit build-related update to EC2

--- src/cfn.py
+++ src/cfn.py
@@ -53,7 +53,7 @@ def update(stackname, autostart="0", concurrency='serial'):
     instances = _check_want_to_be_running(stackname, strtobool(autostart))
     if not instances:
         return
-    return bootstrap.update_stack(stackname, service_list=[], concurrency=concurrency)
+    return bootstrap.update_stack(stackname, service_list=['ec2'], concurrency=concurrency)
 
 @task
 @timeit
@@ -91,19 +91,29 @@ def update_template(stackname):
         # attempting to apply an empty change set would result in an error
         LOG.info("Nothing to update on CloudFormation")
 
+    # TODO: all of the following could possibly be moved
+    # inside bootstrap.update_stack, if it was smart enough
+    # EC2
     if _are_there_existing_servers(context):
         # the /etc/buildvars.json file may need to be updated
         buildvars.refresh(stackname, context)
         update(stackname)
 
+    # SQS
     if context.get('sqs', {}):
         bootstrap.update_stack(stackname, service_list=['sqs'])
 
+    # S3
+    if context.get('s3', {}):
+        bootstrap.update_stack(stackname, service_list=['s3'])
 
 
 @task
 def update_master():
-    return bootstrap.update_stack(core.find_master(aws.find_region()))
+    return bootstrap.update_stack(
+        core.find_master(aws.find_region()),
+        service_list=['ec2'] # master-server should be a self-contained EC2 instance
+    )
 
 @requires_project
 def generate_stack_from_input(pname, instance_id=None, alt_config=None):
