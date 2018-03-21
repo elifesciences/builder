cf5ea399eb6fcf467ea79cb396fb0d9e02a4b99c
Luke Skibinski <l.skibinski@elifesciences.org>
2018-03-21 11:51:21 +1030

    fixed bvars encoding/decoding issues

--- src/buildercore/bvars.py
+++ src/buildercore/bvars.py
@@ -1,19 +1,16 @@
 from os.path import join
-from io import StringIO
+from io import BytesIO
 import json, base64
 from fabric.api import get
 
 def decode_bvars(contents):
-    return json.loads(base64.b64decode(contents).encode())
+    return json.loads(base64.b64decode(contents).decode())
 
-def encode_bvars(contents):
-    return base64.b64encode(json.dumps(contents).encode())
+def encode_bvars(data):
+    return base64.b64encode(json.dumps(data).encode())
 
 def read_from_current_host():
     "returns the buildvars from the CURRENTLY CONNECTED host"
-    # due to a typo we now have two types of file naming in existence
-    # prefer hyphenated over underscores
-    fname = 'build-vars.json.b64'
-    strbuffer = StringIO()
-    get(join('/etc/', fname), strbuffer)
+    strbuffer = BytesIO()
+    get('/etc/build-vars.json.b64', strbuffer)
     return decode_bvars(strbuffer.getvalue())
