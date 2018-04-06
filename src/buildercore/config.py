bb4f0b99134d331430f77c54d007337d703f9ee9
Luke Skibinski <l.skibinski@elifesciences.org>
2018-04-06 13:28:00 +0930

    translated buildercore.s3 from boto2 to boto3

--- src/buildercore/config.py
+++ src/buildercore/config.py
@@ -101,7 +101,7 @@ logging.getLogger('paramiko.transport').setLevel(logging.ERROR)
 
 # where the builder can write stuff that should persist across installations/users
 # like ec2 instance keypairs
-BUILDER_BUCKET = 'elife-builder'
+BUILDER_BUCKET = ('elife-builder', 'us-east-1')
 KEYPAIR_PREFIX = 'keypairs/'
 CONTEXT_PREFIX = 'contexts/'
 
--- src/buildercore/s3.py
+++ src/buildercore/s3.py
@@ -1,46 +1,56 @@
 import os
-import boto
-from boto import s3
-from boto.s3.key import Key
+import boto3
+from botocore.exceptions import ClientError
 from . import config
-from .utils import isstr
+from .utils import isstr, ensure
 from kids.cache import cache as cached
 from io import IOBase
 import logging
 
 LOG = logging.getLogger(__name__)
 
-def connect_s3():
-    # we'll need to deal with this assumption
-    return s3.connect_to_region('us-east-1')  # Location.USWest2)
+def connect_s3(region='us-east-1'):
+    return boto3.resource('s3', region_name=region)
 
 @cached
 def builder_bucket():
+    "returns connection to the bucket where builder stores templates and credentials."
     try:
-        nom = config.BUILDER_BUCKET
-        return connect_s3().get_bucket(nom)
-    except boto.exception.S3ResponseError as err:
-        LOG.error("got an S3 error attempting to get the builder bucket. have you created it yet?",
-                  extra={'bucket': nom, 'error': err.message})
+        nom, region = config.BUILDER_BUCKET
+        resource = connect_s3(region)
+        bucket = resource.Bucket(nom)
+        ensure(bucket in resource.buckets.all(), "bucket %r in region %r does not exist" % (nom, region))
+        return bucket
+    except ClientError as err:
+        LOG.error("unhandled error attempting to find S3 bucket %r in region %r", nom, region,
+                  extra={'bucket': nom, 'region': region, 'error': err.message})
         raise
 
 def exists(key):
-    return builder_bucket().get_key(key) is not None
+    "predicate, returns True if given key in configured bucket+region exists"
+    try:
+        builder_bucket().Object(key).load()
+        return True
+    except ClientError as err:
+        if err.response['Error']['Code'] == '404':
+            return False
+        raise
 
 def write(key, something, overwrite=False):
     "stream is a file-like object"
     if exists(key) and not overwrite:
         raise KeyError("key %r exists and overwrite==False. refusing to overwrite." % key)
-    k = Key(builder_bucket())
-    k.key = key
-    LOG.info("writing key %s", key, extra={'key': key})
+    k = builder_bucket().Object(key)
+    LOG.info("writing key %r", key, extra={'key': key})
+
+    # http://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Object.put
     if isstr(something):
-        k.set_contents_from_string(something)
+        k.put(Body=something.encode()) # bytes
     elif isinstance(something, IOBase):
-        k.set_contents_from_file(something)
+        k.put(Body=something) # py3 file
+    # TODO: py2 warning
     elif isinstance(something, file):
-        # TODO: py2 warning
-        k.set_contents_from_file(something)
+        k.put(Body=something) # py2 file
     else:
         raise ValueError("boto can't handle value of type %r, just strings and files" % type(something))
 
@@ -52,37 +62,41 @@ def delete(key):
         msg = "you tried to delete a key with a protected prefix"
         LOG.warn(msg, extra={'key': key, 'protected': protected})
         raise ValueError(msg)
-    if exists(key):
-        LOG.info("deleting key %s", key, extra={'key': key})
-        builder_bucket().get_key(key).delete()
-    return not exists(key)
+    if not exists(key):
+        return True
+    LOG.info("deleting key %s", key, extra={'key': key})
+    k = builder_bucket().Object(key).delete()
+    # http://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Object.wait_until_not_exists
+    k.wait_until_not_exists()
+    return True
+
+def validate_prefix(prefix):
+    prefix = prefix.strip()
+    if not prefix or len(prefix) < 2:
+        raise ValueError("invalid prefix: %r" % prefix)
+    if not prefix.endswith('/'):
+        raise ValueError("prefix doesn't start or end in a slash: %r" % prefix)
+    if not prefix.startswith('test/'):
+        raise ValueError("only prefixes starting with /test/ allowed: %r" % prefix)
 
 def delete_contents(prefix):
-    def validate_prefix(prefix):
-        prefix = prefix.strip()
-        if not prefix or len(prefix) < 2:
-            raise ValueError("invalid prefix: %r" % prefix)
-        if not prefix.endswith('/'):
-            raise ValueError("prefix doesn't start or end in a slash: %r" % prefix)
-        if not prefix.startswith('test/'):
-            raise ValueError("only prefixes starting with /test/ allowed: %r" % prefix)
     validate_prefix(prefix)
-    for key in builder_bucket().list(prefix=prefix):
+    # TODO: optimisation here: http://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Bucket.delete_objects
+    for key in builder_bucket().objects.filter(Prefix=prefix):
         LOG.info("deleting key %s", key, extra={'key': key})
-        key.delete()
+        key.delete() # not delete(key) ?
 
 def listing(prefix):
     "returns a list of Key objects starting with given prefix rooted in the builder bucket"
-    return builder_bucket().list(prefix=prefix)
+    return builder_bucket().objects.filter(Prefix=prefix)
 
 def simple_listing(prefix):
     "returns a realized list of the names of the keys from the `list` function. "
-    return [key.name for key in listing(prefix)]
+    return [key.key for key in listing(prefix)]
 
 def download(key, output_path):
-    assert not os.path.exists(output_path), "given output path exists, will not overwrite: %r" % output_path
-    k = builder_bucket().get_key(key)
-    assert k, ("Cannot find %s in bucket %s" % (key, builder_bucket()))
+    ensure(not os.path.exists(output_path), "given output path exists, will not overwrite: %r" % output_path)
+    ensure(exists(key), "key %r not found in bucket %r" % (key, config.BUCKET))
     LOG.info("downloading key %s", key, extra={'key': key})
-    k.get_contents_to_file(open(output_path, 'wb'))
+    builder_bucket().Object(key).download_file(output_path)
     return output_path
