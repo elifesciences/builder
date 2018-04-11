e6c298146878853d8050bd3b84f068abd885ee08
Giorgio Sironi <g.sironi@elifesciences.org>
2018-04-11 15:09:10 +0100

    Porting code coverage check to standard --fail-under

--- .run-tests-ci.sh
+++ .run-tests-ci.sh
@@ -5,13 +5,10 @@ set -e # everything must pass
 envname="${1:-default}"
 
 export PYTHONPATH="src"
-pytest --junitxml=build/pytest-$envname.xml src/tests
-pytest -s --junitxml=build/pytest-$envname.xml src/integration_tests
+coverage run -m pytest \
+    --cov=src \
+    -s \
+    --junitxml=build/pytest-$envname.xml \
+    src/tests src/integration_tests
 
-covered=$(coverage report | grep TOTAL | awk '{print $6}' | sed 's/%//')
-if [ $covered -lt 67 ]; then
-    echo
-    echo "FAILED this project requires at least 67% coverage"
-    echo
-    exit 1
-fi
+coverage report --fail-under=67
