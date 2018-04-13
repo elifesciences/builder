13068682a16853178936ed0bb6a9eff6abce5fca
Giorgio Sironi <g.sironi@elifesciences.org>
2018-04-13 14:25:28 +0100

    Remove vagrant-s3auth plugin

--- update.sh
+++ update.sh
@@ -7,19 +7,6 @@ python .prerequisites.py "$@"
 # remove any old compiled python files
 find src/ -name '*.pyc' -delete
 
-# installs s3 auth plugin so we can pull boxes from a private s3 bucket
-if command -v vagrant > /dev/null; then
-    # 2016-08-05, custom basebox disabled until s3auth plugin fixed
-	touch .no-vagrant-s3auth.flag    
-    if [ ! -f .no-vagrant-s3auth.flag ]; then
-        # vagrant plugin update ... doesn't work apparently
-        # just calling install does
-        vagrant plugin install vagrant-s3auth
-    else
-        echo "* the no-vagrant-s3auth flag has been set. skipping check."
-    fi
-fi
-
 # generate a settings file if one doesn't exist
 if [ ! -e settings.yml ]; then
     echo "* settings.yml not found, creating"
@@ -28,16 +15,5 @@ fi
 
 # activate the venv, recreating if neccessary
 . .activate-venv.sh
-
-# download the basebox from s3 if vagrant is installed
-if command -v vagrant > /dev/null; then
-    # 2016-08-05, custom basebox disabled until s3auth plugin fixed
-    touch .no-install-basebox.flag
-    if [ ! -f .no-install-basebox.flag ]; then
-        vagrant box add s3://elife-builder/boxes/ elifesciences/basebox
-    else
-        echo "* the no-install-basebox flag is set. skipping check"
-    fi
-fi
 
 printf "\n   ◕ ‿‿ ◕   all done\n\n"
