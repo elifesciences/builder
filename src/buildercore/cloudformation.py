1dd4fa9460c49fd5ea6ea395d02da9352f05ae65
Luke Skibinski <l.skibinski@elifesciences.org>
2018-04-18 17:38:18 +0930

    ported cloudformation.py to boto3

--- src/buildercore/cloudformation.py
+++ src/buildercore/cloudformation.py
@@ -1,6 +1,6 @@
 import logging
 import backoff
-import boto
+import botocore
 from . import core, trop
 from .utils import ensure
 
@@ -15,8 +15,8 @@ def render_template(context):
 def _log_backoff(event):
     LOG.warn("Backing off in validating project %s", event['args'][0])
 
-@backoff.on_exception(backoff.expo, boto.connection.BotoServerError, on_backoff=_log_backoff, max_time=30)
+@backoff.on_exception(backoff.expo, botocore.exception.ClientError, on_backoff=_log_backoff, max_time=30)
 def validate_template(pname, rendered_template):
     "remote cloudformation template checks."
-    conn = core.connect_aws_with_pname(pname, 'cfn')
-    return conn.validate_template(rendered_template)
+    conn = core.boto_conn(pname, 'cloudformation', client=True)
+    return conn.validate_template(TemplateBody=rendered_template)
