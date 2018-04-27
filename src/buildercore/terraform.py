8945c9621ccb7e521d4fdcf1301e7abbcebb1a8c
Giorgio Sironi <g.sironi@elifesciences.org>
2018-04-27 16:53:39 +0100

    Need to escape \2 and \3 backreferences to get them to point to HIT|MISS|etc

--- src/buildercore/terraform.py
+++ src/buildercore/terraform.py
@@ -32,7 +32,7 @@ FASTLY_LOG_FORMAT = """{
   "request_user_agent":"%{cstr_escape(req.http.User-Agent)}V",
   "request_accept_language":"%{cstr_escape(req.http.Accept-Language)}V",
   "request_accept_charset":"%{cstr_escape(req.http.Accept-Charset)}V",
-  "cache_status":"%{regsub(fastly_info.state, "^(HIT-(SYNTH)|(HITPASS|HIT|MISS|PASS|ERROR|PIPE)).*", "\\2\\3") }V"
+  "cache_status":"%{regsub(fastly_info.state, "^(HIT-(SYNTH)|(HITPASS|HIT|MISS|PASS|ERROR|PIPE)).*", "\\\\2\\\\3") }V"
 }"""
 
 def render(context):
