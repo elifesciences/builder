0bfd87d8c60e32090a4f2f32467b199998566ff2
Giorgio Sironi <g.sironi@elifesciences.org>
2018-05-11 13:28:54 +0100

    Move constants to fastly module

--- src/buildercore/fastly.py
+++ src/buildercore/fastly.py
@@ -54,3 +54,108 @@ class FastlyCustomVCLSnippet(namedtuple('FastlyCustomVCLSnippet', ['name', 'cont
 
 class FastlyCustomVCLGenerationError(Exception):
     pass
+
+# taken from https://docs.fastly.com/guides/vcl/mixing-and-matching-fastly-vcl-with-custom-vcl#fastlys-vcl-boilerplate
+# Fastly expands #FASTLY macros into generated VCL
+# TODO: extract into file
+MAIN_VCL_TEMPLATE = FastlyVCL.from_string("""
+    sub vcl_recv {
+      #FASTLY recv
+
+      if (req.request != "HEAD" && req.request != "GET" && req.request != "FASTLYPURGE") {
+        return(pass);
+      }
+
+      return(lookup);
+    }
+
+    sub vcl_fetch {
+      #FASTLY fetch
+
+      if ((beresp.status == 500 || beresp.status == 503) && req.restarts < 1 && (req.request == "GET" || req.request == "HEAD")) {
+        restart;
+      }
+
+      if (req.restarts > 0) {
+        set beresp.http.Fastly-Restarts = req.restarts;
+      }
+
+      if (beresp.http.Set-Cookie) {
+        set req.http.Fastly-Cachetype = "SETCOOKIE";
+        return(pass);
+      }
+
+      if (beresp.http.Cache-Control ~ "private") {
+        set req.http.Fastly-Cachetype = "PRIVATE";
+        return(pass);
+      }
+
+      if (beresp.status == 500 || beresp.status == 503) {
+        set req.http.Fastly-Cachetype = "ERROR";
+        set beresp.ttl = 1s;
+        set beresp.grace = 5s;
+        return(deliver);
+      }
+
+      if (beresp.http.Expires || beresp.http.Surrogate-Control ~ "max-age" || beresp.http.Cache-Control ~ "(s-maxage|max-age)") {
+        # keep the ttl here
+      } else {
+        # apply the default ttl
+        set beresp.ttl = 3600s;
+      }
+
+      return(deliver);
+    }
+
+    sub vcl_hit {
+      #FASTLY hit
+
+      if (!obj.cacheable) {
+        return(pass);
+      }
+      return(deliver);
+    }
+
+    sub vcl_miss {
+      #FASTLY miss
+      return(fetch);
+    }
+
+    sub vcl_deliver {
+      #FASTLY deliver
+      return(deliver);
+    }
+
+    sub vcl_error {
+      #FASTLY error
+    }
+
+    sub vcl_pass {
+      #FASTLY pass
+    }
+
+    sub vcl_log {
+      #FASTLY log
+    }""")
+
+CUSTOM_VCL_SNIPPETS = {
+    'gzip-by-regex': FastlyCustomVCLSnippet(
+        name='gzip-by-regex',
+        content="""
+        if ((beresp.status == 200 || beresp.status == 404) && (beresp.http.content-type ~ "(\+json)\s*($|;)" || req.url ~ "\.(css|js|html|eot|ico|otf|ttf|json|svg)($|\?)" ) ) {
+          # always set vary to make sure uncompressed versions dont always win
+          if (!beresp.http.Vary ~ "Accept-Encoding") {
+            if (beresp.http.Vary) {
+              set beresp.http.Vary = beresp.http.Vary ", Accept-Encoding";
+            } else {
+              set beresp.http.Vary = "Accept-Encoding";
+            }
+          }
+          if (req.http.Accept-Encoding == "gzip") {
+            set beresp.gzip = true;
+          }
+        }
+        """,
+        type='fetch'
+    ),
+}
