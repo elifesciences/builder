35082bf9e8bbdd85e6d6235cedcae5e0e27adfa3
Giorgio Sironi <g.sironi@elifesciences.org>
2018-05-11 12:18:06 +0100

    Extract FastlyVCL._find_section_start

--- src/buildercore/terraform.py
+++ src/buildercore/terraform.py
@@ -79,22 +79,27 @@ class FastlyVCL:
         return "\n".join(self._lines)
     
     def insert(self, section, statement):
+        # TODO: if we remove indentation altogether from all lines, this will become a single return value
+        section_start, indentation = self._find_section_start(section)
         lines = list(self._lines)
+        lines.insert(section_start + 1, '')
+        lines.insert(
+            section_start + 1, 
+            '%s  %s' % (indentation, statement)
+        )
+        return FastlyVCL(lines)
+    
+    def _find_section_start(self, section):
         lookup = r"(?P<indentation> +)sub vcl_%s {" % section
         section_start = None
-        for i, line in enumerate(lines):
+        for i, line in enumerate(self._lines):
             m = re.match(lookup, line)
             if m:
                 section_start = i
                 break
         if section_start is None:
             raise FastlyCustomVCLGenerationError("Cannot match %s into main VCL template:\n\n%s" % (lookup, str(self)))
-        lines.insert(section_start + 1, '')
-        lines.insert(
-            section_start + 1, 
-            '%s  %s' % (m.group(1), statement)
-        )
-        return FastlyVCL(lines)
+        return section_start, m.group(1)
 
 # taken from https://docs.fastly.com/guides/vcl/mixing-and-matching-fastly-vcl-with-custom-vcl#fastlys-vcl-boilerplate
 # expands #FASTLY macros into generated VCL
