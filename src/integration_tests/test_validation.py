bd0555b4976e936fd758702d4ffc64cb38ede640
Giorgio Sironi <g.sironi@elifesciences.org>
2018-05-24 13:21:20 +0100

    Separate validation tests

--- src/integration_tests/test_validation.py
+++ src/integration_tests/test_validation.py
@@ -1,3 +1,4 @@
+import pytest
 from tests import base
 from buildercore import cfngen, project
 import logging
@@ -7,27 +8,23 @@ logging.disable(logging.NOTSET) # re-enables logging during integration testing
 
 # Depends on talking to AWS.
 
-class TestValidation(base.BaseCase):
-
+class TestValidationFixtures(base.BaseCase):
     def test_validation(self):
         "dummy projects and their alternative configurations pass validation"
         for pname in project.aws_projects().keys():
             cfngen.validate_project(pname)
 
-    def test_validation_elife_projects(self):
-        "elife projects (and their alternative configurations) that come with the builder pass validation"
-
+class TestValidationElife():
+    def setUp(self):
         # HERE BE DRAGONS
         # resets the testing config.SETTINGS_FILE we set in the base.BaseCase class
-        self.switch_out_test_settings()
+        base.switch_out_test_settings()
 
-        for pname in project.aws_projects().keys():
-            # TODO: subTest doesn't work very well here because there's no way to filter a single pname when re-running after a failure
-            # a solution like
-            # https://github.com/elifesciences/elife-spectrum/blob/master/spectrum/test_article.py#L15-L19
-            # could help, but require to run tests with pytest only
-            with self.subTest(pname):
-                cfngen.validate_project(pname)
+    def tearDown(self):
+        base.switch_in_test_settings()
 
-        # todo: does this need to live in a try: ... finally: ... ?
-        self.switch_in_test_settings()
+    @pytest.mark.parametrize("pname", project.aws_projects().keys())
+    def test_validation_elife_projects(self, pname):
+        "elife projects (and their alternative configurations) that come with the builder pass validation"
+
+        cfngen.validate_project(pname)
