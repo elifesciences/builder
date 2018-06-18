ee3190c3c5aa34ed5d723db85643481d203a5b12
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-18 16:13:56 +0100

    Force test or elife settings in test_validation

--- src/tests/base.py
+++ src/tests/base.py
@@ -35,6 +35,10 @@ def test_projects():
     switch_in_test_settings()
     return project.aws_projects().keys()
 
+def elife_projects():
+    switch_out_test_settings()
+    return project.aws_projects().keys()
+
 class BaseCase(TestCase):
     maxDiff = None
 
