be338438e4aa5b804fcdd0a35a373b46957c78bc
Luke <lsh-0@users.noreply.github.com>
2018-10-10 12:05:38 +1030

    adds node parameter to 'upload_file' task adds a default destination if one not specified

--- src/cfn.py
+++ src/cfn.py
@@ -1,3 +1,4 @@
+import os
 from distutils.util import strtobool as _strtobool  # pylint: disable=import-error,no-name-in-module
 from pprint import pformat
 import backoff
@@ -300,9 +301,10 @@ def download_file(stackname, path, destination='.', node=None, allow_missing="Fa
 
 @task
 @requires_aws_stack
-def upload_file(stackname, local_path, remote_path, overwrite=False):
-    with stack_conn(stackname):
-        print('stack:', stackname)
+def upload_file(stackname, local_path, remote_path=None, overwrite=False, node=1):
+    remote_path = remote_path or os.path.join("/tmp", os.path.basename(local_path))
+    with stack_conn(stackname, node=node):
+        print('stack:', stackname, 'node', node)
         print('local:', local_path)
         print('remote:', remote_path)
         print('overwrite:', overwrite)
