fee0eb047162177b431646daf6abad74760aa663
Giorgio Sironi <g.sironi@elifesciences.org>
2018-10-24 14:14:38 +0100

    Stub load_context to avoid dependency on S3/filesystem in test

--- src/tests/test_cfn.py
+++ src/tests/test_cfn.py
@@ -12,17 +12,19 @@ class TestCfn(base.BaseCase):
 
     @patch('cfn.local')
     @patch('buildercore.core.active_stack_names')
+    @patch('buildercore.context_handler.load_context')
     @patch('buildercore.core.find_ec2_instances')
-    def test_ssh_task(self, find_ec2_instances, active_stack_names, local):
-        self._dummy_instance_is_active(find_ec2_instances, active_stack_names)
+    def test_ssh_task(self, find_ec2_instances, load_context, active_stack_names, local):
+        self._dummy_instance_is_active(find_ec2_instances, load_context, active_stack_names)
         ssh('dummy1--prod')
         local.assert_called_with('ssh elife@54.54.54.54 -i ~/.ssh/id_rsa')
 
     @patch('cfn.local')
     @patch('buildercore.core.active_stack_names')
+    @patch('buildercore.context_handler.load_context')
     @patch('buildercore.core.find_ec2_instances')
-    def test_owner_ssh_task(self, find_ec2_instances, active_stack_names, local):
-        self._dummy_instance_is_active(find_ec2_instances, active_stack_names)
+    def test_owner_ssh_task(self, find_ec2_instances, load_context, active_stack_names, local):
+        self._dummy_instance_is_active(find_ec2_instances, load_context, active_stack_names)
         owner_ssh('dummy1--prod')
         (args, _) = local.call_args
         self.assertRegex(args[0], 'ssh ubuntu@54.54.54.54 -i .+/.cfn/keypairs/dummy1--prod.pem')
@@ -33,8 +35,13 @@ class TestCfn(base.BaseCase):
         self.assertEqual(generate_stack_from_input('dummy2', 'alt-config1'), 'dummy2--alt-config1')
         self.assertEqual(generate_stack_from_input('dummy2', 'end2end', alt_config='alt-config1'), 'dummy2--end2end')
 
-    def _dummy_instance_is_active(self, find_ec2_instances, active_stack_names):
+    def _dummy_instance_is_active(self, find_ec2_instances, load_context, active_stack_names):
         active_stack_names.return_value = ['dummy1--prod']
+        load_context.return_value = {
+            'ec2': {
+                'cluster-size': 1,
+            },
+        }
         instance = MagicMock()
         instance.public_ip_address = '54.54.54.54'
         instance.tags = [{'Key': 'Name', 'Value': 'dummy1--test--1'}]
