dd74978fb0f4a0d474d8c1ec07de0ecd3374a290
Giorgio Sironi <g.sironi@elifesciences.org>
2018-11-12 15:58:36 +0000

    Stub context in memory in lifecycle tests

--- src/tests/test_buildercore_lifecycle.py
+++ src/tests/test_buildercore_lifecycle.py
@@ -1,14 +1,14 @@
-import json
 from datetime import datetime
 from mock import patch, MagicMock
 from pytz import utc
 from . import base
 from buildercore.core import parse_stackname
-from buildercore import cfngen, context_handler, lifecycle
+from buildercore import cfngen, lifecycle
 
 
 class TestBuildercoreLifecycle(base.BaseCase):
     def setUp(self):
+        self.contexts = {}
         self._generate_context('dummy1--test')
         self._generate_context('project-with-rds-only--test')
 
@@ -16,7 +16,9 @@ class TestBuildercoreLifecycle(base.BaseCase):
     @patch('buildercore.lifecycle._ec2_connection')
     @patch('buildercore.lifecycle.find_rds_instances')
     @patch('buildercore.lifecycle.find_ec2_instances')
-    def test_start_a_not_running_ec2_instance(self, find_ec2_instances, find_rds_instances, ec2_connection, some_node_is_not_ready):
+    @patch('buildercore.lifecycle.load_context')
+    def test_start_a_not_running_ec2_instance(self, load_context, find_ec2_instances, find_rds_instances, ec2_connection, some_node_is_not_ready):
+        load_context.return_value = self.contexts['dummy1--test']
         find_ec2_instances.side_effect = [
             [self._ec2_instance('stopped')],
             [self._ec2_instance('running')],
@@ -32,7 +34,9 @@ class TestBuildercoreLifecycle(base.BaseCase):
     @patch('buildercore.lifecycle._rds_connection')
     @patch('buildercore.lifecycle.find_rds_instances')
     @patch('buildercore.lifecycle.find_ec2_instances')
-    def test_start_a_not_running_rds_instance(self, find_ec2_instances, find_rds_instances, rds_connection):
+    @patch('buildercore.lifecycle.load_context')
+    def test_start_a_not_running_rds_instance(self, load_context, find_ec2_instances, find_rds_instances, rds_connection):
+        load_context.return_value = self.contexts['project-with-rds-only--test']
         find_ec2_instances.return_value = []
         find_rds_instances.side_effect = [
             [self._rds_instance('stopped')],
@@ -47,18 +51,24 @@ class TestBuildercoreLifecycle(base.BaseCase):
 
     @patch('buildercore.lifecycle.find_rds_instances')
     @patch('buildercore.lifecycle.find_ec2_instances')
-    def test_start_ec2_idempotence(self, find_ec2_instances, find_rds_instances):
+    @patch('buildercore.lifecycle.load_context')
+    def test_start_ec2_idempotence(self, load_context, find_ec2_instances, find_rds_instances):
+        load_context.return_value = self.contexts['dummy1--test']
         find_ec2_instances.return_value = [self._ec2_instance('running')]
         lifecycle.start('dummy1--test')
 
     @patch('buildercore.lifecycle.find_rds_instances')
     @patch('buildercore.lifecycle.find_ec2_instances')
-    def test_start_rds_idempotence(self, find_ec2_instances, find_rds_instances):
+    @patch('buildercore.lifecycle.load_context')
+    def test_start_rds_idempotence(self, load_context, find_ec2_instances, find_rds_instances):
+        load_context.return_value = self.contexts['project-with-rds-only--test']
         find_rds_instances.return_value = [self._rds_instance('available')]
         lifecycle.start('project-with-rds-only--test')
 
     @patch('buildercore.lifecycle.find_ec2_instances')
-    def test_ignores_terminated_stacks_that_have_been_replaced_by_a_new_instance(self, find_ec2_instances):
+    @patch('buildercore.lifecycle.load_context')
+    def test_ignores_terminated_stacks_that_have_been_replaced_by_a_new_instance(self, load_context, find_ec2_instances):
+        load_context.return_value = self.contexts['dummy1--test']
         old = self._ec2_instance('terminated', 'i-123')
         new = self._ec2_instance('running', 'i-456')
 
@@ -68,7 +78,9 @@ class TestBuildercoreLifecycle(base.BaseCase):
     @patch('buildercore.lifecycle._stop')
     @patch('buildercore.lifecycle.find_rds_instances')
     @patch('buildercore.lifecycle.find_ec2_instances')
-    def test_stops_ec2_instance(self, find_ec2_instances, find_rds_instances, _stop):
+    @patch('buildercore.lifecycle.load_context')
+    def test_stops_ec2_instance(self, load_context, find_ec2_instances, find_rds_instances, _stop):
+        load_context.return_value = self.contexts['dummy1--test']
         find_ec2_instances.return_value = [self._ec2_instance('running')]
         find_rds_instances.return_value = []
         lifecycle.stop('dummy1--test')
@@ -76,7 +88,9 @@ class TestBuildercoreLifecycle(base.BaseCase):
     @patch('buildercore.lifecycle._stop')
     @patch('buildercore.lifecycle.find_rds_instances')
     @patch('buildercore.lifecycle.find_ec2_instances')
-    def test_stops_rds_instance(self, find_ec2_instances, find_rds_instances, _stop):
+    @patch('buildercore.lifecycle.load_context')
+    def test_stops_rds_instance(self, load_context, find_ec2_instances, find_rds_instances, _stop):
+        load_context.return_value = self.contexts['project-with-rds-only--test']
         find_ec2_instances.return_value = []
         find_rds_instances.return_value = [self._rds_instance('available')]
         lifecycle.stop('project-with-rds-only--test')
@@ -91,7 +105,7 @@ class TestBuildercoreLifecycle(base.BaseCase):
     def _generate_context(self, stackname):
         (pname, instance_id) = parse_stackname(stackname)
         context = cfngen.build_context(pname, stackname=stackname)
-        context_handler.write_context_locally(stackname, json.dumps(context))
+        self.contexts[stackname] = context
 
     def _ec2_instance(self, state='running', id='i-456', launch_time=datetime(2017, 1, 1, tzinfo=utc)):
         instance = MagicMock()
