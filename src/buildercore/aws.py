faaca51073993729838f1f31754dd3f18402cfd1
Giorgio Sironi <g.sironi@elifesciences.org>
2019-04-10 09:05:12 +0100

    Use actual original order

--- src/buildercore/aws.py
+++ src/buildercore/aws.py
@@ -6,8 +6,8 @@ def generic_tags(context, name=True):
     tags = OrderedDict([
         ('Project', context['project_name']), # "journal"
         ('Environment', context['instance_id']), # "prod"
-        ('Cluster', context['stackname']), # "journal--prod"
         # the name AWS Console uses to label an instance
         ('Name', context['stackname']), # "journal--prod"
+        ('Cluster', context['stackname']), # "journal--prod"
     ])
     return tags
