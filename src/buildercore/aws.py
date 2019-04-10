5c7db252f17081375e77562aff887203ce189047
Giorgio Sironi <g.sironi@elifesciences.org>
2019-04-10 08:52:48 +0100

    Specify accidental old order of AWS tags

--- src/buildercore/aws.py
+++ src/buildercore/aws.py
@@ -7,7 +7,7 @@ def generic_tags(context, name=True):
         ('Project', context['project_name']), # "journal"
         ('Environment', context['instance_id']), # "prod"
         ('Cluster', context['stackname']), # "journal--prod"
+        # the name AWS Console uses to label an instance
+        ('Name', context['stackname']), # "journal--prod"
     ])
-    # the name AWS Console uses to label an instance
-    tags['Name'] = context['stackname'] # "journal--prod"
     return tags
