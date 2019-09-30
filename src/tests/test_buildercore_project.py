6d04ff3f92b11173af4dc860c0f1518b73008fdf
Luke <lsh-0@users.noreply.github.com>
2019-09-30 17:27:00 +0930

    rds, adds upgrade-major-version to list of expected test projects

--- src/tests/test_buildercore_project.py
+++ src/tests/test_buildercore_project.py
@@ -11,7 +11,8 @@ ALL_PROJECTS = [
     'project-with-ec2-custom-root', 'project-with-ec2-t2-unlimited',
     'project-with-cluster', 'project-with-cluster-suppressed', 'project-with-cluster-overrides', 'project-with-cluster-empty',
     'project-with-stickiness', 'project-with-multiple-elb-listeners',
-    'project-with-cluster-integration-tests', 'project-with-db-params', 'project-with-rds-only', 'project-with-rds-encryption', 'project-with-elasticache-redis', 'project-with-multiple-elasticaches', 'project-with-fully-overridden-elasticaches',
+    'project-with-cluster-integration-tests', 'project-with-db-params', 'project-with-rds-only', 'project-with-rds-encryption', 'project-with-rds-major-version-upgrade',
+    'project-with-elasticache-redis', 'project-with-multiple-elasticaches', 'project-with-fully-overridden-elasticaches',
     'project-on-gcp', 'project-with-bigquery-datasets-only', 'project-with-bigquery', 'project-with-bigquery-remote-schemas',
     'project-with-eks', 'project-with-eks-helm',
 ]
