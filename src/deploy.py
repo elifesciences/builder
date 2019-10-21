babce93c443aa1be6dcb99a9b8ae127c1c4a4690
Giorgio Sironi <g.sironi@elifesciences.org>
2019-10-21 11:46:22 +0100

    Improve load balancers debug tasks
    - Output of instances that are attached and/or healthy
    - "Register all" task with waiting condition

--- src/deploy.py
+++ src/deploy.py
@@ -1,9 +1,12 @@
 """module concerns itself with tasks involving branch deployments of projects."""
 
+from pprint import pformat
 from fabric.api import task
 from decorators import requires_aws_stack, debugtask
+# TODO: import modules rather than functions
 from buildercore import bootstrap, cloudformation, context_handler
-from buildercore.core import boto_client
+from buildercore.bluegreen import BlueGreenConcurrency
+from buildercore.core import boto_client, all_node_params
 from buildercore.concurrency import concurrency_for
 import buildvars
 
@@ -21,10 +24,21 @@ def switch_revision_update_instance(stackname, revision=None, concurrency='seria
 @requires_aws_stack
 def load_balancer_status(stackname):
     context = context_handler.load_context(stackname)
+    # TODO: delegate to BlueGreenConcurrency?
     elb_name = cloudformation.read_output(stackname, 'ElasticLoadBalancer')
     conn = boto_client('elb', context['aws']['region'])
     health = conn.describe_instance_health(
         LoadBalancerName=elb_name,
     )['InstanceStates']
     LOG.info("Load balancer name: %s", elb_name)
-    LOG.info("Health: %s", health)
+    LOG.info("Health: %s", pformat(health))
+
+@debugtask
+@requires_aws_stack
+def load_balancer_register_all(stackname):
+    context = context_handler.load_context(stackname)
+    elb_name = cloudformation.read_output(stackname, 'ElasticLoadBalancer')
+    concurrency = BlueGreenConcurrency(context['aws']['region'])
+    node_params = all_node_params(stackname)
+    concurrency.register(elb_name, node_params)
+    concurrency.wait_registered_all(elb_name, node_params)
