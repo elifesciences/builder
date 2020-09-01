d94970ed6a6736207410aa2d8722d32d5399c860
Luke <lsh-0@users.noreply.github.com>
2020-09-01 18:27:08 +0930

    update-dependencies script now requires python3.6 to generate lock file this fixes an issue where transitive dependencies that don't support 3.6 were being installed

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # updates Pipfile.lock and regenerates the requirements.txt file
-
-set -ex
+set -e
 
 # create/update existing venv
-. .activate-venv.sh
+rm -rf venv/
+
+# whatever your preferred version of python is, builder needs to support python3.6 (Ubuntu 18.04)
+python3.6 -m venv venv
 
 # updates the Pipfile.lock file and then installs the newly updated dependencies.
 # the envvar is necessary otherwise pipenv will use it's own .venv directory.
