fbb90fc508e0c802d4d946d283864e9cc42e9951
Luke <lsh-0@users.noreply.github.com>
2020-10-29 11:34:46 +1030

    cryptography issue GHSA-hggm-jpg3-v476

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -8,6 +8,10 @@ rm -rf venv/
 # whatever your preferred version of python is, eLife needs to support python3.6 (Ubuntu 18.04)
 python3.6 -m venv venv
 
+# prefer using wheels to compilation
+source venv/bin/activate
+pip install wheel 
+
 # updates the Pipfile.lock file and then installs the newly updated dependencies.
 # the envvar is necessary otherwise pipenv will use it's own .venv directory.
 VIRTUAL_ENV="venv" pipenv update --dev
