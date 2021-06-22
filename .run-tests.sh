e68c3850270e427c844d3d555c998e334bd2fefe
Luke <lsh-0@users.noreply.github.com>
2021-06-22 14:52:31 +0930

    .run-tests.sh, fixes bug where passing more args caused a bash warning

--- .run-tests.sh
+++ .run-tests.sh
@@ -2,13 +2,13 @@
 # ignores integration tests.
 # see `.run-tests-ci.sh` for running the integration testing.
 # called by `test.sh` and `canary.sh`
-
 set -e
+args="$@"
 
 # `patched_pytest` is a copy of 'pytest' but with gevent monkey patching.
 # see `venv/bin/pytest` and `src/buildercore/threadbare/__init__.py`
 
-if [ -z "$@" ]; then
+if [ -z "$args" ]; then
     # no arguments given, run the regular test suite and ignore integration tests
     ./patched_pytest src/tests/ -vv "$@"
 else
