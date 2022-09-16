2f6c6000c752899464dbc42445027fb97b532362
Luke <lsh-0@users.noreply.github.com>
2022-09-16 20:14:22 +0930

    (adds missing file)

---
+++ src/aws/ec2.py
@@ -0,0 +1,7 @@
+def restart(stackname, node):
+    """You probably want 'lifecycle.restart'.
+    Restarts an ec2 instance, polling until available.
+    Good for kicking individual stuck instances without affecting all
+    nodes in cluster."""
+    #
+    pass
