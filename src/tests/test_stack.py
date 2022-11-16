12ab2a7286496a93f38981c061a7f8d91db44679
Luke <lsh-0@users.noreply.github.com>
2022-11-16 14:11:08 +1030

    stacks, disabled test with a pointer that was moved elsewhere

--- src/tests/test_stack.py
+++ src/tests/test_stack.py
@@ -12,5 +12,6 @@ def test_generate_stacks__unknown_config_file():
     with pytest.raises(utils.TaskExit):
         stack.generate_stacks('s3-bucket', '/foo/bar/baz')
 
-def test_generate_stacks():
-    stack.generate_stacks('s3-bucket', '/home/luke/dev/python/builder-private-stack-config/aws-s3-buckets.yaml')
+# see: src/tests/test_buildercore_project_stack_generation.py
+#def test_generate_stacks():
+#    stack.generate_stacks('s3-bucket', ...
