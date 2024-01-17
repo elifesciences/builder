d47bdf5d78fa61f2fc4305030432bc03e503947e
Luke <2142748+lsh-0@users.noreply.github.com>
2024-01-17 12:01:20 +1030

    askmaster, removed chunk of unused code

--- src/askmaster.py
+++ src/askmaster.py
@@ -10,7 +10,19 @@ from buildercore.core import stack_conn
 
 def salt_master_cmd(cmd, module='cmd.run', minions=r'\*'):
     """runs the given command on all connected minions.
-    given command must escape double quotes."""
+    given command must escape double quotes.
+
+    example `minions` strings:
+
+    '*'                 # all minions
+    'lax--*'            # all 'lax' minions
+    'lax--prod--*'      # all 'lax--prod' minions
+    '*--prod--*'        # all 'prod' minions
+    '*--prod--* or *--continuumtest--*' # all 'prod' and 'continuumtest' minions
+    "--compound 'G@osrelease:18.04'" # 18.04+ minions only
+
+    see: https://docs.saltproject.io/en/latest/topics/targeting/"""
+
     with stack_conn(core.find_master(utils.find_region())):
         remote_sudo("salt %(minions)s %(module)s %(cmd)s --timeout=30" % locals())
 
@@ -36,13 +48,3 @@ def linux_distro():
     """returns the version of the OS
     looks like: 'Description:    Ubuntu 18.04.1 LTS'"""
     return salt_master_cmd("'lsb_release -d'")
-
-# lsh@2020-09: disabled, unused. good for reference.
-# def update_kernel():
-#    "updates the kernel on all minions using a specific ubuntu release"
-#    cmd = "'apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install linux-image-aws -y'"
-#    # 18.04+ minions only
-#    minion_set = [
-#        'G@osrelease:18.04',
-#    ]
-#    [salt_master_cmd(cmd, minions="--compound '%s'" % m) for m in minion_set]
